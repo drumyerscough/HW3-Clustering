52c7be670250f742bea5ff8f68019135b592eeb3
drumyerscough <68093667+drumyerscough@users.noreply.github.com>
2024-01-28 18:31:25 -0800

    implement kmeans and silhouette scoring

--- cluster/kmeans.py
+++ cluster/kmeans.py
@@ -20,6 +20,15 @@ class KMeans:
             max_iter: int
                 the maximum number of iterations before quitting model fit
         """
+        if k <= 1:
+            raise ValueError("This k doesn't make sense. k should be an integer greater than 1.")
+        
+        self._k = k
+        self._tol = tol
+        self._max_iter = max_iter
+
+        self._mu = None
+        self._error = None
 
     def fit(self, mat: np.ndarray):
         """
@@ -36,6 +45,34 @@ class KMeans:
             mat: np.ndarray
                 A 2D matrix where the rows are observations and columns are features
         """
+        if self._k >= len(mat):
+            raise Exception('This dataset is smaller than the number of clusters k.')
+        elif mat.dtype != float and mat.dtype != int:
+            raise Exception('The input matrix contains non-numerical data. KMeans only works on numerical data.')
+        
+        num_iter = 0
+        sse = np.inf
+        delta_sse = np.inf
+        #mu = np.random.rand(self._k, mat.shape[1])
+        mu = mat[np.random.randint(mat.shape[0], size=self._k), :]
+
+        while num_iter < self._max_iter and delta_sse > self._tol:
+            r = cdist(mat, mu)
+            #r = np.argmin(r, axis=1, keepdims=True)
+            r = np.equal(r, np.min(r, axis=1, keepdims=True))
+            mu = np.matmul(r.T,mat) / np.sum(r, axis=0, keepdims=True).T
+
+            new_sse = np.sum(cdist(mat, mu)**2 * r)
+            if num_iter == 0:
+                delta_sse = new_sse
+            else:
+                delta_sse = new_sse - sse
+            sse = new_sse
+            num_iter += 1
+
+        self._mu = mu
+        self._error = sse
+        
 
     def predict(self, mat: np.ndarray) -> np.ndarray:
         """
@@ -53,6 +90,12 @@ class KMeans:
             np.ndarray
                 a 1D array with the cluster label for each of the observations in `mat`
         """
+        if mat.dtype != float and mat.dtype != int:
+            raise Exception('The input matrix contains non-numerical data. KMeans only works on numerical data.')
+        elif self._mu.all() == None:
+            raise Exception("This instance of KMeans hasn't been fit yet.")
+
+        return np.argmin(cdist(mat, self._mu), axis=1)
 
     def get_error(self) -> float:
         """
@@ -63,6 +106,10 @@ class KMeans:
             float
                 the squared-mean error of the fit model
         """
+        if self._error == None:
+            raise Exception("This instance of KMeans hasn't been fit yet.")
+
+        return self._error
 
     def get_centroids(self) -> np.ndarray:
         """
@@ -72,3 +119,7 @@ class KMeans:
             np.ndarray
                 a `k x m` 2D matrix representing the cluster centroids of the fit model
         """
+        if self._mu == None:
+            raise Exception("This instance of KMeans hasn't been fit yet.")
+
+        return self._mu
--- cluster/silhouette.py
+++ cluster/silhouette.py
@@ -24,3 +24,27 @@ class Silhouette:
             np.ndarray
                 a 1D array with the silhouette scores for each of the observations in `X`
         """
+        if X.dtype != float and X.dtype != int:
+            raise ValueError('X contains non-numerical data. Silhouette scoring with Euclidean distance only works on numerical data.')
+        if X.shape[0] != y.shape[0]:
+            raise Exception('X and y are different lengths.')
+        
+        # convert unique labels in y to numbers, then to 1-hot matrix
+        lbl_map = {lbl: idx for idx, lbl in enumerate(np.unique(y))}
+        y_sanitized = list(map(lambda x: lbl_map[x], y))
+        y_1hot = np.eye(len(lbl_map))[y_sanitized]
+
+        # compute average intracluster distances
+        dists = cdist(X, X)
+        a = np.sum(dists*np.matmul(y_1hot,y_1hot.T), axis=0) / \
+            np.sum((y_1hot * (np.sum(y_1hot, axis=0).T - 1).T), axis=1)
+        
+        # compute average distances to other clusters and take the minimum
+        b = np.min(
+            np.matmul(dists, y_1hot) / np.sum(y_1hot, axis=0), 
+            axis=1, initial=np.inf, where=(y_1hot == 0))
+
+        # compute silhouette scores
+        s = (b - a) / np.maximum(a, b)
+
+        return s
